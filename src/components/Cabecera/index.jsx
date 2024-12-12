950b90736b9a61ef33e01e3c3388de626c07cb19
sriveraar <phognus@hotmail.com>
2024-12-12 18:01:02 -0300

    Initial commit

---
+++ src/components/Cabecera/index.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+import CampoTexto from "../CampoTexto"
+
+const HeaderEstilizado = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img{
+        width: 212px;
+    }
+`
+
+const Cabecera = () =>{
+    return <HeaderEstilizado>
+        <img src="img/logo.png" alt="Logo de Space APP" />
+        <CampoTexto />
+    </HeaderEstilizado>
+}
+
+export default Cabecera
